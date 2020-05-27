13ddba22b16918ee7518ff252681d16b7686e550
Evan Beh <evan@togalimited.com>
2020-05-27 13:33:23 +0800

    Initial Commit

---
+++ CarTrackTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarTrackTest
+//
+//  Created by Evan Beh on 27/05/2020.
+//  Copyright © 2020 Evan Beh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
